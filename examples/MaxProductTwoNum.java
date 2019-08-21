42f85797c8a428630f9d7101cf93298a1b38fee8
DSK <durgasuresh_kvsd@yahoo.in>
2019-08-21 17:23:46 +0530

    adding java examples to bahskar repo

---
+++ examples/MaxProductTwoNum.java
@@ -0,0 +1,17 @@
+public class MaxProductTwoNum()
+{
+	public static void main(String [] args)
+	{
+		Stack<Integer> stc = new Stack<Integer>
+		stc.push(3);
+		stc.push(5);
+		stc.push(6);
+		stc.push(7);
+		stc.push(8);
+		System.out.println("Stack : " + stc);
+	}
+}
+
+		
+		
+		

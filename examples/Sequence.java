74ed2cbaa5326bb5ccfaa52871b989a3860aaec3
Bhaskar <bhaskar221998@gmail.com>
2019-09-24 23:55:45 +0530

    adding pattern-sequnce programs

---
+++ examples/Sequence.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Sequence {
+	public static void main(String [] args)
+	{
+	
+		for(int i=90;i>=85;i--)
+		{
+			 for (int j=85;j<=i;j++) 
+			 {
+				 
+				System.out.print(j + " ");
+			} 
+			System.out.println(" " );
+		}
+	}
+}
